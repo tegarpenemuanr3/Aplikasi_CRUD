f3a20dfecb840e8df8238b087c473e97c436b191
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2022-10-30 23:33:43 +0700

    Show Data

--- app/src/main/java/com/tegarpenemuan/aplikasicrud/MainActivity.kt
+++ app/src/main/java/com/tegarpenemuan/aplikasicrud/MainActivity.kt
@@ -2,10 +2,42 @@ package com.tegarpenemuan.aplikasicrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.tegarpenemuan.aplikasicrud.model.ProductsResponse
+import com.tegarpenemuan.aplikasicrud.retrofit.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tv_response: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_response = findViewById(R.id.tv_show)
+        showdata()
+    }
+
+    private fun showdata() {
+
+
+        RetrofitClient.instance.getProducts().enqueue(object : Callback<ProductsResponse> {
+            override fun onResponse(
+                call: Call<ProductsResponse>,
+                response: Response<ProductsResponse>
+            ) {
+                Log.d("TAG","data: "+response.body().toString())
+                tv_response.text = response.body().toString()
+            }
+
+            override fun onFailure(call: Call<ProductsResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
